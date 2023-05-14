e23a35f78ade7a5b5fae9cdd5b4cc12121751aba
Nick Loza <nicklozadeveloper@gmail.com>
2023-05-14 14:27:44 -0300

    add: link to repository

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,9 +1,15 @@
 import Chats from "./Chats"
-
+import  {AiOutlineGithub } from "react-icons/ai"
 function Aside() {
 	return (
 		<aside className="home__aside">
 			<Chats/>
+			<a 
+			className="home__aside-repo"
+			href="https://github.com/nickLoza/chat-app-world" 
+			target="_blank">
+				<AiOutlineGithub/> <span>Github</span>
+			</a>
 		</aside>
 	)
 }
